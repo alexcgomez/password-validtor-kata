89369a3326abafe035e63c927ab53a7b8ab1f574
Alex Cáceres <alexcaceres93@gmail.com>
2022-10-25 19:18:41 +0200

    Lower case test in GREEN

--- src/PasswordValidator.ts
+++ src/PasswordValidator.ts
@@ -1,5 +1,5 @@
 export class PasswordValidator {
   public validate(password: string): boolean {
-    return password.length > 8 && password.match(/[A-Z]/) !== null;
+    return password.length > 8 && password.match(/[A-Z]/) !== null && password.match(/[a-z]/) !== null;
   }
 }
