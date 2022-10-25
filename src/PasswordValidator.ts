c19d1a3d744a189abad408a3e1487aaf8f89dd5b
Alex Cáceres <alexcaceres93@gmail.com>
2022-10-25 19:21:05 +0200

    Underscore case test in GREEN

--- src/PasswordValidator.ts
+++ src/PasswordValidator.ts
@@ -3,6 +3,7 @@ export class PasswordValidator {
     return password.length > 8 &&
       password.match(/[A-Z]/) !== null &&
       password.match(/[a-z]/) !== null &&
-      password.match(/[0-9]/) !== null;
+      password.match(/[0-9]/) !== null &&
+      password.match(/_/) !== null;
   }
 }
